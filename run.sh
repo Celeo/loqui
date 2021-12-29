2551dc853c06abfd4d023321856dde62f1bcf311
Celeo <mattboulanger@fastmail.com>
2021-12-28 20:15:34 -0800

    Restrict net permission

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deno run --allow-net --allow-read=data.db,data.db-journal --allow-write=data.db,data.db-journal --unstable main.ts
+deno run --allow-net=127.0.0.1,deno.land --allow-read=data.db,data.db-journal --allow-write=data.db,data.db-journal --unstable main.ts
